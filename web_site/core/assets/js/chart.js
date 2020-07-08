50036186987541fee760a66bfe6258cea0c68f1f
Wei Tung Chen <weitungchen1998@gmail.com>
2020-07-08 13:58:03 -0700

    Added scatter plot

--- web_site/core/assets/js/chart.js
+++ web_site/core/assets/js/chart.js
@@ -1,8 +1,45 @@
-var chartTitle = 'Title';
-var xAxisTitle = 'Time';
-var yAxisTitle = 'Val';
 
-function onLoad(series) {
+var graphConfig = {
+	1: {
+		chartTitle: 'Time Series Plot',
+		xAxisTitle: 'Time',
+		yAxisTitle: 'Val',
+	},
+	2: {
+		chartTitle: 'Scatter Plot',
+		xAxisTitle: 'Val1',
+		yAxisTitle: 'Val2',
+	}
+};
+
+function graph1(text) {
+	let { chartTitle, xAxisTitle, yAxisTitle } = graphConfig[1];
+
+	// Parse
+	let series = [
+		{
+			name: 'Dataset1',
+			data: [],
+		},
+		{
+			name: 'Dataset2',
+			data: [],
+		}
+	];
+
+	let data = text.split('\n');
+	data.shift();
+	data.forEach(line => {
+		let vals = line.split(',');
+		let time = new Date(vals[0]);
+		let val1 = parseFloat(vals[1]) || undefined;
+		let val2 = parseFloat(vals[2]) || undefined;
+
+		series[0].data.push([time, val1]);
+		series[1].data.push([time, val2]);
+	});
+
+	// Build
 	$('#container').highcharts('StockChart', {
 		chart: {
 			type: 'scatter',
@@ -70,16 +107,13 @@ function onLoad(series) {
 	});
 }
 
-fetch('https://raw.githubusercontent.com/brianemery/baseline_website/master/test_data/test_series.csv').then(async res => {
-	let text = await res.text();
+function graph2(text) {
+	let { chartTitle, xAxisTitle, yAxisTitle } = graphConfig[2];
 
+	// Parse
 	let series = [
 		{
 			name: 'Dataset1',
-			data: [],
-		},
-		{
-			name: 'Dataset2',
 			data: [],
 		}
 	];
@@ -88,13 +122,62 @@ fetch('https://raw.githubusercontent.com/brianemery/baseline_website/master/test
 	data.shift();
 	data.forEach(line => {
 		let vals = line.split(',');
-		let time = new Date(vals[0]);
 		let val1 = parseFloat(vals[1]) || undefined;
 		let val2 = parseFloat(vals[2]) || undefined;
 
-		series[0].data.push([time, val1]);
-		series[1].data.push([time, val2]);
+		series[0].data.push([val1, val2]);
 	});
 
-	onLoad(series);
+	series.push({
+        type: 'line',
+        name: 'Regression Line',
+        data: [[-53, -43], [56, 42]],
+        marker: {
+            enabled: false
+        },
+        states: {
+            hover: {
+                lineWidth: 0
+            }
+        },
+        enableMouseTracking: false
+    });
+
+	// Build
+	$('#container2').highcharts({
+		chart: {
+			type: 'scatter',
+			plotBorderColor: 'black',
+			plotBorderWidth: 2,
+		},
+		title: {
+			text: chartTitle,
+			AxisTypeValue: 'linear'
+		},
+		xAxis: {
+			title: {
+				text: xAxisTitle
+			},
+		},
+		yAxis: {
+			title: {
+				text: yAxisTitle
+			},
+			opposite: false,
+			minorTickInterval: undefined,
+		},
+		legend: {
+			enabled: true
+		},
+		navigator: {
+			enabled: false
+		},
+		series: series
+	});
+}
+
+fetch('https://raw.githubusercontent.com/brianemery/baseline_website/master/test_data/test_series.csv').then(async res => {
+	let text = await res.text();
+	graph1(text);
+	graph2(text);
 });
