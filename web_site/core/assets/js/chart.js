3ddcf89b2e710607d0d18c2b6abeae549ce9c047
Wei Tung Chen <weitungchen1998@gmail.com>
2020-07-10 19:11:37 -0700

    added linked functionality to scatter + bug fix

--- web_site/core/assets/js/chart.js
+++ web_site/core/assets/js/chart.js
@@ -1,157 +1,239 @@
+const dataUrl = 'https://raw.githubusercontent.com/brianemery/baseline_website/master/test_data/test_series.csv';
 
-var graphConfig = {
-	1: {
-		chartTitle: 'Time Series Plot',
-		xAxisTitle: 'Time',
-		yAxisTitle: 'Val',
-	},
-	2: {
-		chartTitle: 'Scatter Plot',
-		xAxisTitle: 'Val1',
-		yAxisTitle: 'Val2',
+class Graph {
+	constructor(dataUrl, containers) {
+		this.timeSeries = null;
+		this.scatterPlot = null;
+		this.scatterData = [];
+		this.scatterTime = [];
+		[ this.timeSeriesContainer, this.scatterPlotContainer ] = containers;
+
+		fetch(dataUrl).then(async res => {
+			let text = await res.text();
+
+			let data = text.split('\n');
+			let header = data.shift().split(',');
+			data.pop();
+
+			this.graphTimeSeries(data, header);
+			this.graphScatterPlot(data, header);
+		});
 	}
-};
 
-function graph1(text) {
-	let { chartTitle, xAxisTitle, yAxisTitle } = graphConfig[1];
+	graphTimeSeries(data, header) {
+		let chartTitle = 'Time Series Plot';
 
-	// Parse
-	let series = [
-		{
-			name: 'Dataset1',
-			data: [],
-		},
-		{
-			name: 'Dataset2',
-			data: [],
-		}
-	];
-
-	let data = text.split('\n');
-	data.shift();
-	data.forEach(line => {
-		let vals = line.split(',');
-		let time = new Date(vals[0]);
-		let val1 = parseFloat(vals[1]) || undefined;
-		let val2 = parseFloat(vals[2]) || undefined;
-
-		series[0].data.push([time, val1]);
-		series[1].data.push([time, val2]);
-	});
-
-	// Build
-	$('#container').highcharts('StockChart', {
-		chart: {
-			plotBorderColor: 'black',
-			plotBorderWidth: 2,
-		},
-		title: {
-			text: chartTitle
-		},
-		xAxis: {
-			type: 'datetime',
-			title: {
-				text: xAxisTitle
+		// Parse
+		let series = [
+			{
+				name: header[1],
+				data: [],
 			},
-			minorTickInterval: 'auto',
-			// startOnTick: true,
-			// endOnTick: true
-		},
-		yAxis: {
-			title: {
-				text: yAxisTitle
-			},
-			opposite: false,
-			minorTickInterval: undefined,
-		},
-		legend: {
-			enabled: true
-		},
-		rangeSelector: {
-			// enabled: true,
-			// selected: 2
-		},
-		navigator: {
-			enabled: false
-		},
-		rangeSelector: {
-			selected: 1,
-		},
-		series: series
-	});
-}
-
-function graph2(text) {
-	let { chartTitle, xAxisTitle, yAxisTitle } = graphConfig[2];
-
-	// Parse
-	let series = [
-		{
-			name: 'Dataset1',
-			data: [],
-		}
-	];
-
-	let data = text.split('\n');
-	data.shift();
-	data.forEach(line => {
-		let vals = line.split(',');
-		let val1 = parseFloat(vals[1]) || undefined;
-		let val2 = parseFloat(vals[2]) || undefined;
-
-		series[0].data.push([val1, val2]);
-	});
-
-	series.push({
-		type: 'line',
-		name: 'Regression Line',
-		data: [[-53, -43], [56, 42]],
-		marker: {
-			enabled: false
-		},
-		states: {
-			hover: {
-				lineWidth: 0
+			{
+				name: header[2],
+				data: [],
 			}
-		},
-		enableMouseTracking: false
-	});
+		];
 
-	// Build
-	$('#container2').highcharts({
-		chart: {
-			type: 'scatter',
-			plotBorderColor: 'black',
-			plotBorderWidth: 2,
-		},
-		title: {
-			text: chartTitle,
-			AxisTypeValue: 'linear'
-		},
-		xAxis: {
-			title: {
-				text: xAxisTitle
+		data.forEach(line => {
+			let vals = line.split(',');
+			let time = new Date(vals[0]);
+			let val1 = parseFloat(vals[1]) || 0;
+			let val2 = parseFloat(vals[2]) || 0;
+
+			series[0].data.push([time, val1]);
+			series[1].data.push([time, val2]);
+		});
+
+
+		// Build
+		Highcharts.setOptions({
+		    lang:{
+		        rangeSelectorZoom: ''
+		    },
+		});
+
+		this.timeSeries = Highcharts.stockChart(this.timeSeriesContainer, {
+			chart: {
+				plotBorderColor: 'black',
+				plotBorderWidth: 2,
+				events: {
+					redraw: () => this.onRedraw()
+				}
 			},
-		},
-		yAxis: {
 			title: {
-				text: yAxisTitle
+				text: chartTitle,
 			},
-			opposite: false,
-			minorTickInterval: undefined,
-		},
-		legend: {
-			enabled: true
-		},
-		navigator: {
-			enabled: false
-		},
-		series: series
-	});
+			xAxis: {
+				type: 'datetime',
+				title: {
+					text: header[0]
+				},
+				dateTimeLabelFormats: {
+				    minute: '%l %p',
+				    day:    '%b %e',
+				    week:   '%b %e',
+				    month:  '%b \'%y',
+				    year:   '%Y'
+				},
+				minorTickInterval: 'auto',
+				// startOnTick: true,
+				// endOnTick: true
+				minRange: 60 * 1000,
+			},
+			yAxis: {
+				title: {
+					text: 'cm/s'
+				},
+				opposite: false,
+				minorTickInterval: undefined,
+			},
+			legend: {
+				enabled: true
+			},
+			rangeSelector: {
+				// enabled: true,
+				selected: 0,
+				buttons: [{
+	                type: 'day',
+	                count: 1,
+	                text: '1 Day',
+	                dataGrouping: {
+	                    forced: true,
+	                    units: [['minute', [1]]]
+	                }
+	            },{
+	                type: 'week',
+	                count: 1,
+	                text: '1 Week',
+	                dataGrouping: {
+	                    forced: true,
+	                    units: [['minute', [1]]]
+	                }
+	            },{
+	                type: 'month',
+	                count: 1,
+	                text: ' 1 Month ',
+	                dataGrouping: {
+	                    forced: true,
+	                    units: [['hour', [1]]]
+	                }
+	            },{
+	                type: 'month',
+	                count: 3,
+	                text: ' 3 Months ',
+	                dataGrouping: {
+	                    forced: true,
+	                    units: [['hour', [1]]]
+	                }
+	            }],
+
+	            buttonTheme: {
+	                width: 70,
+	            }
+			},
+			navigator: {
+				enabled: false
+			},
+			series: series
+		});
+	}
+
+	graphScatterPlot(data, header) {
+		let chartTitle = 'Scatter Plot';
+
+		let minDate = this.timeSeries.xAxis[0].min;
+		let maxDate = this.timeSeries.xAxis[0].max;
+		let index = 0;
+
+		data.forEach(line => {
+			let vals = line.split(',');
+			let time = new Date(vals[0]);
+			let x = parseFloat(vals[1]) || undefined;
+			let y = parseFloat(vals[2]) || undefined;
+
+			if (x != null && y != null) {
+				this.scatterData.push({
+					x: x,
+					y: y,
+					visible: time < maxDate && time > minDate,
+				});
+
+				this.scatterTime[index++] = time;
+			}
+		});
+
+		// Parse
+		let series = [
+			{
+				name: 'Dataset1',
+				type: 'scatter',
+				turboThreshold: 0,
+				data: this.scatterData
+			}
+		];
+
+		series.push({
+			type: 'line',
+			name: 'Regression Line',
+			data: [[-53, -43], [56, 42]],
+			marker: {
+				enabled: false
+			},
+			states: {
+				hover: {
+					lineWidth: 0
+				}
+			},
+			enableMouseTracking: false
+		});
+
+
+		// Build
+		this.scatterPlot = Highcharts.chart(this.scatterPlotContainer, {
+			chart: {
+				type: 'scatter',
+				plotBorderColor: 'black',
+				plotBorderWidth: 2,
+			},
+			title: {
+				text: chartTitle,
+				AxisTypeValue: 'linear'
+			},
+			xAxis: {
+				title: {
+					text: header[1]
+				},
+			},
+			yAxis: {
+				title: {
+					text: header[2]
+				},
+				opposite: false,
+				minorTickInterval: undefined,
+			},
+			legend: {
+				enabled: true
+			},
+			navigator: {
+				enabled: false
+			},
+			series: series
+		});
+	}
+
+	async onRedraw() {
+		let minDate = this.timeSeries.xAxis[0].min;
+		let maxDate = this.timeSeries.xAxis[0].max;
+
+		this.scatterData.forEach((point, i) => {
+			point.visible = this.scatterTime[i] < maxDate && this.scatterTime[i] > minDate;
+		});
+
+		this.scatterPlot.series[0].setData(this.scatterData);
+	}
 }
 
-fetch('https://raw.githubusercontent.com/brianemery/baseline_website/master/test_data/test_series.csv').then(async res => {
-	let text = await res.text();
-	graph1(text);
-	graph2(text);
-});
+new Graph(dataUrl, ['container1', 'container2']);
+// new Graph(dataUrl, ['container3', 'container4']);
+// new Graph(dataUrl, ['container5', 'container6']);
